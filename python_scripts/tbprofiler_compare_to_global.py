5c071982874b1f229bebef2aaa5e01b7afe2cc40
GaryNapier <garynapier83@googlemail.com>
2021-11-24 18:32:29 +0000

    not sure

--- python_scripts/tbprofiler_compare_to_global.py
+++ python_scripts/tbprofiler_compare_to_global.py
@@ -11,6 +11,9 @@ import csv
 import pathogenprofiler as pp
 import tbprofiler
 
+
+
+
 def resolve_lineages(data):
     data = dict(data)
     while True:
@@ -52,6 +55,19 @@ def main(args):
     else:
         global_samples = [x.replace(args.suffix,"") for x in os.listdir(args.global_dir) if x[-len(args.suffix):]==args.suffix]
 
+    # Get the gene, change, drug combinations for each sample.
+    # Example:
+    # {('katG', 'p.Ser315Thr', 'isoniazid'): ['SRR1723521'],
+    #  ('gyrA', 'p.Glu21Gln', 'NA'): ['SRR1723521'],
+    #  ('gyrA', 'p.Ser95Thr', 'NA'): ['SRR1723521'],
+    #  ('gyrA', 'p.Gly668Asp', 'NA'): ['SRR1723521'],
+    #  ('mshA', 'p.Ala187Val', 'NA'): ['SRR1723521'],
+    #  ('rpsL', 'c.-165T>C', 'NA'): ['SRR1723521'],
+    #  ('katG', 'p.Arg463Leu', 'NA'): ['SRR1723521'],
+    #  ('ahpC', 'c.-101A>G', 'NA'): ['SRR1723521'],
+    #  ('thyA', 'p.Pro3Ala', 'NA'): ['SRR1723521'],
+    #  ('ald', 'c.-32T>C', 'NA'): ['SRR1723521'],
+    #  ('gid', 'p.Glu92Asp', 'NA'): ['SRR1723521']})
     project_variants = defaultdict(list)
     for s in tqdm(project_samples):
         data = json.load(open(pp.filecheck("%s/%s%s" % (args.project_dir,s,args.suffix))))
@@ -65,6 +81,51 @@ def main(args):
             else:
                 project_variants[(var["gene"],var["change"],"NA")].append(s)
 
+    # Same for global samples, except no filter for synonymous, no 'drug' and also get the gene-change combination for lineage:
+    # defaultdict(list,
+    #         {('katG', 'p.Ser315Thr'): ['SRR1723521'],
+    #          ('gyrA', 'p.Glu21Gln'): ['SRR1723521'],
+    #          ('gyrA', 'p.Ser95Thr'): ['SRR1723521'],
+    #          ('gyrA', 'p.Gly668Asp'): ['SRR1723521'],
+    #          ('fgd1', 'c.960T>C'): ['SRR1723521'],
+    #          ('mshA', 'p.Ala187Val'): ['SRR1723521'],
+    #          ('rpoB', 'c.3225T>C'): ['SRR1723521'],
+    #          ('rpsL', 'c.-165T>C'): ['SRR1723521'],
+    #          ('rpsA', 'c.636A>C'): ['SRR1723521'],
+    #          ('tlyA', 'c.33A>G'): ['SRR1723521'],
+    #          ('katG', 'p.Arg463Leu'): ['SRR1723521'],
+    #          ('ahpC', 'c.-101A>G'): ['SRR1723521'],
+    #          ('thyA', 'c.666G>C'): ['SRR1723521'],
+    #          ('thyA', 'p.Pro3Ala'): ['SRR1723521'],
+    #          ('ald', 'c.-32T>C'): ['SRR1723521'],
+    #          ('embC', 'c.2781C>T'): ['SRR1723521'],
+    #          ('embA', 'c.228C>T'): ['SRR1723521'],
+    #          ('gid', 'c.615T>C'): ['SRR1723521'],
+    #          ('gid', 'p.Glu92Asp'): ['SRR1723521']})
+
+    # and
+
+        # defaultdict(list,
+    #         {('katG', 'p.Ser315Thr'): ['lineage2.2.1'],
+    #          ('gyrA', 'p.Glu21Gln'): ['lineage2.2.1'],
+    #          ('gyrA', 'p.Ser95Thr'): ['lineage2.2.1'],
+    #          ('gyrA', 'p.Gly668Asp'): ['lineage2.2.1'],
+    #          ('fgd1', 'c.960T>C'): ['lineage2.2.1'],
+    #          ('mshA', 'p.Ala187Val'): ['lineage2.2.1'],
+    #          ('rpoB', 'c.3225T>C'): ['lineage2.2.1'],
+    #          ('rpsL', 'c.-165T>C'): ['lineage2.2.1'],
+    #          ('rpsA', 'c.636A>C'): ['lineage2.2.1'],
+    #          ('tlyA', 'c.33A>G'): ['lineage2.2.1'],
+    #          ('katG', 'p.Arg463Leu'): ['lineage2.2.1'],
+    #          ('ahpC', 'c.-101A>G'): ['lineage2.2.1'],
+    #          ('thyA', 'c.666G>C'): ['lineage2.2.1'],
+    #          ('thyA', 'p.Pro3Ala'): ['lineage2.2.1'],
+    #          ('ald', 'c.-32T>C'): ['lineage2.2.1'],
+    #          ('embC', 'c.2781C>T'): ['lineage2.2.1'],
+    #          ('embA', 'c.228C>T'): ['lineage2.2.1'],
+    #          ('gid', 'c.615T>C'): ['lineage2.2.1'],
+    #          ('gid', 'p.Glu92Asp'): ['lineage2.2.1']})
+
     global_variants = defaultdict(list)
     global_drtypes = defaultdict(list)
     global_variant_lineages = defaultdict(list)
@@ -73,21 +134,31 @@ def main(args):
         for var in data["dr_variants"] + data["other_variants"]:
             global_variants[(var["gene"],var["change"])].append(s)
             global_variant_lineages[(var["gene"],var["change"])].append(data["sublin"])
+
+        # Store the DR type for each samp:
+        # Example:
+        # defaultdict(list, {'Pre-MDR': ['SRR1723521']})
         global_drtypes[data["drtype"]].append(s)
+    
     meta = {}
     for row in csv.DictReader(open(args.meta)):
         meta[row["wgs_id"]] = row
 
     def calculate_stats(gene,change,drug,dr_pos,project_freq):
 
+        # Get the samples that have the combination of gene & change from the global variants list
         g_samples = set(global_variants[(gene,change)])
+        # Extract the DST status (0/1) as numeric values, ignoring missing values
+        # Final DST freq will be proportion of sensitive and susceptible for just those samples that have been tested. 
         global_dsts = [int(meta[s][drug]) for s in g_samples if meta[s][drug]!="NA"] if drug in list(list(meta.values())[0]) else []
+        # Store in dict
         res = {
             "gene":gene,"change":change,"drug":drug,"dr_pos":dr_pos,
             "project_freq":project_freq,
-            "global_freq":len(g_samples),
+            "global_N":len(g_samples),
             "drug_resistant_%":"%.2f" % (sum(global_dsts)/len(global_dsts)) if len(global_dsts)!=0 else "NA"
         }
+        # Loop over DR types and get the number of samples in each category
         for drtype in ["Sensitive","Pre-MDR","MDR","Pre-XDR","XDR","Other"]:
             samps = global_drtypes[drtype]
             res[drtype+"_freq"] = sum([1 for s in samps if s in g_samples])/len(samps)
@@ -95,11 +166,24 @@ def main(args):
 
     results = []
     for gene,change,drug in tqdm(project_variants):
+        # Get the freq for each gene,change,drug in the project (subset the dict on the gene,change,drug and get the number of samples (len))
         project_freq = len(project_variants[(gene,change,drug)])
+
+        # If the gene is not a drug resistance-associated one, then make this the results dict entry:
         if gene not in gene2drugs:
-            results.append({"gene":gene,"change":change,"drug":"NA","dr_pos":"No","project_freq":project_freq,"global_freq":"NA","drug_resistant_%":"NA"})
+            results.append({"gene":gene, 
+            "change":change, 
+            "drug":"NA",
+            "dr_pos":"No",
+            "project_freq":project_freq,
+            "global_freq":"NA",
+            "drug_resistant_%":"NA"})
+        
+        # If the drug is "NA" (the CHANGE is non-DR-associated), then... 
         elif drug=="NA":
+            # look up the gene and get the drug(s)
             possible_drugs = gene2drugs[gene]
+            # Loop over the drug(s) and 
             for d in possible_drugs:
                 var = calculate_stats(gene,change,d,"No",project_freq)
                 var["lineages"] = resolve_lineages(Counter(global_variant_lineages[(gene,change)]))
@@ -118,12 +202,12 @@ def main(args):
 
 # Set up the parser
 parser = argparse.ArgumentParser(description='tbprofiler script',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--project-samples',type=str,help='File with samples')
+# parser.add_argument('--project-samples',type=str,help='File with samples')
 parser.add_argument('--global-samples',type=str,help='File with samples')
-parser.add_argument('--out',type=str,help='Directory containing results',required=True)
-parser.add_argument('--meta',type=str,help='Directory containing results',required=True)
-parser.add_argument('--project-dir',type=str,help='Directory containing results',required=True)
-parser.add_argument('--global-dir',type=str,help='Directory containing results',required=True)
+parser.add_argument('--out',type=str,help='directory for results',required=True)
+parser.add_argument('--meta',type=str,help='metadata file',required=True)
+# parser.add_argument('--project-dir',type=str,help='Directory containing results',required=True)
+parser.add_argument('--global-dir',type=str,help='location of tbprofiler json files',required=True)
 parser.add_argument('--db',default="tbdb",type=str,help='Database name')
 parser.add_argument('--suffix',default=".results.json",type=str,help='File suffix')
 parser.set_defaults(func=main)
