6cdfdbdf02a98a7cf8321a4aa652676c0fdd7614
GaryNapier <garynapier83@googlemail.com>
2022-02-22 21:43:39 +0000

    latest

--- shell_scripts/comp_mut_master_script.sh
+++ shell_scripts/comp_mut_master_script.sh
@@ -17,52 +17,66 @@ cd ~/comp_mut
 # ----------
 
 # Directories
+# ------------
 
 # Existing directories
 tbp_results_dir=/mnt/storage7/jody/tb_ena/tbprofiler/freebayes/results/
 main_metadata_dir=../metadata/
 local_metadata_dir=metadata/
+database_dir=../pipeline/db/
+results_dir=results/
 
 # Files
+# ------
 
 # Existing files
 main_tb_metadata_file=${main_metadata_dir}tb_data_18_02_2021.csv
+known_comp_mut_file=${database_dir}compensatory_mutations.csv 
 
 # Created files
-
 head_metadata_file=${main_metadata_dir}head_metadata.csv # testing
 sample_list=test_sample_list.txt
 temp_json_files_list=files.tmp
-# find_ahpc_mutations.py
-novel_ahpc_mutations_file=${local_metadata_dir}novel_ahpc_mutations.txt
-# ahpc_mut_glm.R
-ahpc_model_results_file=${local_metadata_dir}ahpc_model_results.csv
+# find_comp_mutations.py files
+novel_comp_mut_data_file=${results_dir}novel_ahpc_mutations.txt
+# filter_comp_mut.R files
+model_results_file=${results_dir}ahpc_model_results.csv
+
+# Variables 
+drug_of_interest=isoniazid
+id_key=wgs_id
 
 
 # ------------------------
-# Find all ahpC mutations
+# Find all comp mutations
 # ------------------------
 
-if [ ! -f ${novel_ahpc_mutations_file} ]; then
-echo " --- RUNNING python_scripts/find_ahpc_mutations.py --- "
-python python_scripts/find_ahpc_mutations.py --metadata-file ${main_tb_metadata_file} --id-key wgs_id --tbp-results ${tbp_results_dir} --outfile ${novel_ahpc_mutations_file}
+if [ ! -f ${novel_comp_mut_data_file} ]; then
+echo " --- PULLING COMPENSATORY MUTATIONS DATA FOR ${drug_of_interest} RUNNING python_scripts/find_novel_comp_mutations.py --- "
+python python_scripts/find_novel_comp_mutations.py \
+--metadata-file ${main_tb_metadata_file} \
+--known-comp-mut-file ${known_comp_mut_file}
+--drug-of-interest ${drug_of_interest} \
+--id-key ${wgs_id} \
+--tbp-results ${tbp_results_dir} \
+--outfile ${novel_comp_mut_data_file}
 fi
 
-# ------------------------------------------------------------------------------------------------------------
-# Run ahpc_mut_glm.R to run models to see if DST is significantly predicted against each mutation and lineage
-# ------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------
+# Run filter_comp_mut.R - run GLM models to see if DST is significantly predicted against presence of each mutation and lineage
+# ------------------------------------------------------------------------------------------------------------------------------
 
-if [ ! -f ${ahpc_model_results_file} ]; then
-echo " --- RUNNING r_scripts/ahpc_mut_glm.R --- "
-Rscript r_scripts/ahpc_mut_glm.R --metadata_file ${main_tb_metadata_file} --mutations_data_file ${novel_ahpc_mutations_file} --outfile ${ahpc_model_results_file}
+if [ ! -f ${model_results_file} ]; then
+echo " --- FILTERING POTENTIAL NEW COMPENSATORY MUTATIONS - RUNNING r_scripts/filter_comp_mut.R --- "
+Rscript r_scripts/filter_comp_mut.R \
+--metadata_file ${main_tb_metadata_file} \
+--drug_of_interest ${drug_of_interest}\
+--novel_comp_mut_data_file ${novel_comp_mut_data_file} \
+--outfile ${model_results_file} 
 fi
 
 # -----------------------------------------------------------
-# Run tbprofiler_template.py to find novel mutations in katG
+
 # -----------------------------------------------------------
 
-
-# python python_scripts/tbprofiler_template_testing.py --sample_list_file sample_list --dir ../pakistan/tbprofiler_pakistan_results/json
-
-# python python_scripts/tbprofiler_template.py --dir ${tbp_results_dir}
 
