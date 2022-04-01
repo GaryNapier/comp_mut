320d309bbffcc77cdb3ca54c9c8cbc8e6b3066aa
GaryNapier <garynapier83@googlemail.com>
2022-04-01 15:01:49 +0100

    add testing if else; add annotation; adjust save size

--- r_scripts/comp_mut_tree.R
+++ r_scripts/comp_mut_tree.R
@@ -254,7 +254,7 @@ if (TESTING){
   final_tree
 }else{
   # Save
-  ggsave(outfile, final_tree, width = 1100, height = 700, units = "px")
+  ggsave(outfile, final_tree, width = 1100, height = 700, units = "mm")
 }
 
 
