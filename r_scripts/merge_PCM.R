957a6d5fcd0d1469214c91557955e3275ff7775f
Gary Napier <garynapier@Garys-MacBook-Pro-2.local>
2022-06-06 19:14:31 +0100

    simple merge of PCM list and KCM list

--- r_scripts/merge_PCM.R
+++ r_scripts/merge_PCM.R
@@ -38,6 +38,7 @@
 
 TESTING <- 0
 
+library(dplyr)
 library(optparse)
 
 source("https://raw.githubusercontent.com/GaryNapier/Packages_functions/master/Functions.R")
@@ -46,10 +47,11 @@ if (TESTING){
   # TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING TESTING 
   setwd("~/Documents/comp_mut/")
   drug_of_interest <- 'isoniazid'
-  tc_file <- paste0("metadata/", drug_of_interest, "_tc.txt")
-  PCM_data_file <- paste0("results/", drug_of_interest, "_PCM_data.txt")
+  # tc_file <- paste0("metadata/", drug_of_interest, "_tc.txt")
+  # PCM_data_file <- paste0("results/", drug_of_interest, "_PCM_data.txt")
   KCM_file <- "../pipeline/db/compensatory_mutations.csv"
-  # outfile <- "results/isoniazid_PCM_merged.csv"
+  PCM_file <- "metadata/found_PCM.csv"
+  outfile <- sprintf("results/%s_PCM_KCM_merged.csv", drug_of_interest)
 }else{
   
   # Arguments ----
@@ -57,11 +59,13 @@ if (TESTING){
   option_list = list(
     make_option(c("-d", "--drug_of_interest"), type="character", default=NULL,
                 help="drug of interest", metavar="character"),
-    make_option(c("-t", "--tc_file"), type="character", default=NULL,
-                help="file of results for drug of interest from TC", metavar="character"),
-    make_option(c("-g", "--PCM_data_file"), type="character", default=NULL,
-                help="file of results for drug of interest from find_PCM.py", metavar="character"),
-    make_option(c("-c", "--KCM_file"), type="character", default=NULL,
+    # make_option(c("-t", "--tc_file"), type="character", default=NULL,
+    #             help="file of results for drug of interest from TC", metavar="character"),
+    # make_option(c("-g", "--PCM_data_file"), type="character", default=NULL,
+    #             help="file of results for drug of interest from find_PCM.py", metavar="character"),
+    make_option(c("-k", "--KCM_file"), type="character", default=NULL,
+                help="file of known compensatory mutations", metavar="character"),
+    make_option(c("-p", "--PCM_file"), type="character", default=NULL,
                 help="file of known compensatory mutations", metavar="character"),
     make_option(c("-o", "--outfile"), type="character", default=NULL,
                 help="name of outfile", metavar="character")
@@ -76,45 +80,54 @@ if (TESTING){
   print(str(opt))
   
   drug_of_interest <- opt$drug_of_interest
-  tc_file <- opt$tc_file
-  PCM_data_file <- opt$PCM_data_file
+  # tc_file <- opt$tc_file
+  # PCM_data_file <- opt$PCM_data_file
   KCM_file <- opt$KCM_file
+  PCM_file <- opt$PCM_file
   outfile <- opt$outfile
 }
 
 
-tc_results <- read.table(tc_file, header = T)
-PCM_data <- read.table(PCM_data_file, header = T)
+# tc_results <- read.table(tc_file, header = T)
+# PCM_data <- read.table(PCM_data_file, header = T)
 KCM <- read.csv(KCM_file, header = T)
+PCM <- read.csv(PCM_file, header = T)
 
 # Wrangle
-tc_results <- round_cols(tc_results)
-tc_results <- tc_results[tc_results["waldp"] < 0.05, ]
+# tc_results <- round_cols(tc_results)
+# tc_results <- tc_results[tc_results["waldp"] < 0.05, ]
+# 
+# tc_results <- tc_results[, c("gene2", "pos2")]
+# names(tc_results) <- c("gene", "change")
 
-tc_results <- tc_results[, c("gene2", "pos2")]
-names(tc_results) <- c("gene", "change")
+# PCM_data <- PCM_data[, c("gene", "change")]
 
-PCM_data <- PCM_data[, c("gene", "change")]
+# Put the KCM and PCM together
+KCM <- select(subset(KCM, Drug == drug_of_interest), Gene, Mutation)
+PCM <- select(subset(PCM, drug == drug_of_interest), gene, mutation)
+CM <- rbind_force(KCM, PCM)
+names(CM) <- c('gene', 'change')
 
-results <- unique(rbind(tc_results, PCM_data))
-results <- odr(results)
+# results <- unique(rbind(tc_results, PCM_data))
+# results <- odr(results)
 
-# Get genes for drug of interest in KCM list
-KCM_genes <- unique(KCM[KCM["Drug"] == drug_of_interest, "Gene"])
-
-# Filter out any results that are not in the KCM genes list
-results <- results[results[, "gene"] %in% KCM_genes, ]
+# # Get genes for drug of interest in KCM list
+# KCM_genes <- unique(KCM[KCM["Drug"] == drug_of_interest, "Gene"])
+# 
+# # Filter out any results that are not in the KCM genes list
+# results <- results[results[, "gene"] %in% KCM_genes, ]
 
 # Filter ahpC mutations
 if(drug_of_interest == "isoniazid"){
   
   # Remove non-promoters from ahpC PCM
-  results <- results[!(grepl("p.", results$change)), ]
+  # results <- results[!(grepl("p.", results$change)), ]
+  CM <- CM[!(grepl("p.", CM$change)), ]
   
 }
 
 if (!TESTING){
-  write.csv(results, file = outfile, quote = F, row.names = F)
+  write.csv(CM, file = outfile, quote = F, row.names = F)
 }
 
 
