9be2f8ce7f6668b1ee448d7eeb7140377c8377a5
GaryNapier <garynapier83@googlemail.com>
2022-06-06 18:12:12 +0000

    changed to take simple list of merged PCM and KCM

--- python_scripts/comp_mut2res_mut.py
+++ python_scripts/comp_mut2res_mut.py
@@ -141,19 +141,21 @@ def main(args):
 
     # FILES
 
-    PCM_file = args.PCM_file
+    # in 
+    CM_file = args.CM_file
+    KCM_file = args.KCM_file
     metadata_file = args.metadata_file
     tbdb_file = args.tbdb_file
     CM_RM_assoc_file = args.CM_RM_assoc_file
     drtypes_file = args.drtypes_file
-    KCM_file = args.KCM_file
     tbprofiler_results_dir = args.tbprofiler_results_dir
     vars_exclude_file = args.vars_exclude_file
+    # out
+    CM_stats_file = args.CM_stats_file
     PRM_stats_file = args.PRM_stats_file
-    PCM_stats_file = args.PCM_stats_file
     PRM_samples_file = args.PRM_samples_file
     binary_table_file = args.binary_table_file
-    summary_file = args.summary_file
+    # summary_file = args.summary_file
 
     # VARIABLES
 
@@ -164,27 +166,29 @@ def main(args):
     # READ IN DATA
 
     # Read in potential CM results file
-    with open(PCM_file, 'r') as f:
+    with open(CM_file, 'r') as f:
         reader = csv.DictReader(f)        
-        PCM_list = []
+        CM_list = []
         for row in reader:
-            PCM_list.append((row['gene'], row['change']))
+            CM_list.append((row['gene'], row['change']))
 
     # Read in metadata
     with open(metadata_file) as mf:
         meta_dict = csv_to_dict(mf)
-        
+
     # Pull samples
     samples = list(meta_dict.keys())
 
     # # Read in tbdb file
     with open(tbdb_file, 'r') as f:
         tbdb_dict = csv_to_dict_multi(f, 'Drug')
-    
+
     # Read in CM-RM gene association file and store genes
     CM_RM_assoc_file
     with open(CM_RM_assoc_file, 'r') as f:
         CM_RM_assoc_dict = csv_to_dict_multi(f, 'drug')
+
+    # WRANGLE DATA
 
     RM_genes = set()
     CM_genes = set()
@@ -200,14 +204,9 @@ def main(args):
     for row in csv.DictReader(open(KCM_file)):
         if row['Drug'] != drug_of_interest: continue
         KCM[row['Drug']].add((row['Gene'],row['Mutation']))
-    ################################################################
-    n_KCM = len(KCM[drug_of_interest])
-    ################################################################
     
     # Read in variants to exclude
     vars_exclude = get_vars_exclude(vars_exclude_file, do_lineage)
-
-    # WRANGLE DATA
 
     # Find genes associated with drug of interest
     genes = set()
@@ -216,7 +215,7 @@ def main(args):
 
     # Concat with genes from known and potential resistance mutations
     genes = genes.union(set([var[0] for var in KCM[drug_of_interest]]))
-    genes = genes.union(set([var[0] for var in PCM_list]))
+    genes = genes.union(set([var[0] for var in CM_list]))
 
     # Read in all the json data for (samples with) mutations in drug-of-interest genes only (>0.7 freq) and the metadata for those samples
 
@@ -241,7 +240,7 @@ def main(args):
             if ';' in data['sublin']: continue
             # Skip animal samples
             if 'La' in data['main_lin']: continue
-            # Just do lineages 7, 8, 9
+            # Skip lineages 7, 8, 9
             if data['main_lin'] in {'lineage7', 'lineage8', 'lineage9'}: continue
 
             # MAKE SURE THE FOR LOOP BELOW IS INDENTED IN LINE WITH if os.path.isfile(file):
@@ -258,7 +257,7 @@ def main(args):
                 if "drugs" in var:
                     for d in var["drugs"]:
                         if d["drug"] not in drug_of_interest: continue
-                        if key in KCM[d["drug"]] or key in PCM_list: continue
+                        if key in CM_list: continue
                         resistance_mutations[d["drug"]].add(key)
 
     # Clean up samples list - needs to agree with samples which actually have files
@@ -267,33 +266,21 @@ def main(args):
     # Clean up meta_dict - needs to agree with samples which actually have files
     meta_dict = {samp: meta_dict[samp] for samp in samples}
 
-    # Classify potential compensatory mutations and filter 
-    # Need to check against tbprofiler results for each mutation 
-    # First remove PCMs that are in the KCM list
-    PCM_list = [var for var in PCM_list if var not in KCM[drug_of_interest]]
-    ########################################################
-    n_PCM_before_filtering = len(PCM_list) 
-    ########################################################
-    PCM_filtered, PCM_stats = filter_vars(PCM_list, mutation2sample, meta_dict, drug_of_interest, CM_genes, do_lineage)
+    # Filter all CM
+    CM_filtered, CM_stats = filter_vars(CM_list, mutation2sample, meta_dict, drug_of_interest, CM_genes, do_lineage)
 
     print()
-    print("FOUND %s POTENTIAL COMPENSATORY MUTATIONS FOR %s" % (len(PCM_filtered), drug_of_interest))
+    print("USING %s COMPENSATORY MUTATIONS FOR %s" % (len(CM_filtered), drug_of_interest))
     print()
 
-    # WRITE PCM FILES
-    with open(PCM_stats_file, 'w') as f:
-        writer = csv.DictWriter(f, fieldnames = list(get_embedded_keys(PCM_stats)))
+    # WRITE FILES
+    with open(CM_stats_file, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames = list(get_embedded_keys(CM_stats)))
         writer.writeheader()
-        for row in PCM_stats:
-            writer.writerow(PCM_stats[row])
-    clean_file(PCM_stats_file)
+        for row in CM_stats:
+            writer.writerow(CM_stats[row])
 
-    # Add the filtered potential compensatory mutations 
-    # to the list of known compensatory mutations for the drug of interest
-    CM = KCM[drug_of_interest].union(PCM_filtered)
-    ############################################################################
-    n_CM_after_filtering = len(CM)
-    ############################################################################
+    clean_file(CM_stats_file)
 
     # ** MAIN BIT **
     # ** Go over all the samples and get the potential resistance mutations from the presence of comp mutations ** 
@@ -302,9 +289,9 @@ def main(args):
     samps_CM = []
     for s in tqdm(samples):
         # Get the comp, res and other variants for each sample in the full sample list
-        comp_var = [var for var in sample2mutation[s] if var in CM]
+        comp_var = [var for var in sample2mutation[s] if var in CM_filtered]
         res_var = [var for var in sample2mutation[s] if var in resistance_mutations[drug_of_interest]]
-        other_vars = [var for var in sample2mutation[s] if var not in CM\
+        other_vars = [var for var in sample2mutation[s] if var not in CM_filtered\
                     and var not in resistance_mutations[drug_of_interest]]
 
         # Make sure there are no RM genes in res_var e.g. all the res_var can be all fabG1 and no katG
@@ -351,19 +338,19 @@ def main(args):
     # CM
     # for samp in sample2mutation:
     for samp in samples:
-        CM_vars = [var for var in sample2mutation[samp] if var in CM]
+        CM_vars = [var for var in sample2mutation[samp] if var in CM_filtered]
         KRM_vars = [var for var in sample2mutation[samp] if var in resistance_mutations[drug_of_interest]]
         PRM_vars = [var for var in sample2mutation[samp] if var in PRM_filtered]
         # Take out promoters in RIF
         if drug_of_interest == 'rifampicin':
             other_vars = [var for var in sample2mutation[samp] \
-                        if var not in CM and \
+                        if var not in CM_filtered and \
                         var not in resistance_mutations[drug_of_interest] and \
                         var not in PRM_filtered and \
                         'c.-' not in var[1]]
         else:
             other_vars = [var for var in sample2mutation[samp] \
-                        if var not in CM and \
+                        if var not in CM_filtered and \
                         var not in resistance_mutations[drug_of_interest] and \
                         var not in PRM_filtered]
 
@@ -381,11 +368,11 @@ def main(args):
     # Split out other vars in case more than one in there
     for samp in binary_table:
         other_vars = binary_table[samp]['other_vars']
-        other_var_genes = [x[0] for x in binary_table[samp]['other_vars']]
+        other_var_genes = [x[0] for x in other_vars]
         for gene in other_var_genes:
             vars_gene = [var for var in other_vars if var[0] == gene]
             binary_table[samp].update({"other_"+gene: vars_gene})
-
+            
     # Split out KRM
     for samp in binary_table:
         KRM_vars = binary_table[samp]['KRM']
@@ -406,64 +393,6 @@ def main(args):
         for key in keys_needed: 
             binary_table[samp].update({key: []})
 
-    # vars
-
-    # CM
-    CM_in_samps = set(flat_list([binary_table[samp]['CM'] for samp in binary_table]))
-    KCM_in_samps = {var for var in CM_in_samps if var in KCM[drug_of_interest]}
-    PCM_in_samps = {var for var in CM_in_samps if var in PCM_filtered}
-    # RM
-    KRM_in_samps = set(flat_list([binary_table[samp]['KRM'] for samp in binary_table]))
-
-    # samps
-    samps_PRM = []
-    samps_CM = []
-    samps_CM_and_KRM = []
-    samps_CM_and_no_KRM = []
-    samps_CM_and_no_KRM_and_PRM = []
-    samps_CM_and_no_KRM_and_no_PRM = []
-    for samp in binary_table:
-        if len(binary_table[samp]['PRM']) >0:
-            samps_PRM.append(samp)
-        if len(binary_table[samp]['CM']) > 0:
-            samps_CM.append(samp)
-            if len(binary_table[samp]['KRM']) > 0:
-                samps_CM_and_KRM.append(samp)
-            if len(binary_table[samp]['KRM']) == 0:
-                samps_CM_and_no_KRM.append(samp)
-                if len(binary_table[samp]['PRM']) > 0:
-                    samps_CM_and_no_KRM_and_PRM.append(samp)
-                if len(binary_table[samp]['PRM']) == 0:
-                    samps_CM_and_no_KRM_and_no_PRM.append(samp)
-
-    summary_dict = {'n_CM_in_list_before_filtering': n_KCM + n_PCM_before_filtering,
-    'n_KCM_in_list': n_KCM, 
-    'n_PCM_in_list_before_filtering': n_PCM_before_filtering,
-                        
-    'n_CM_in_list_after_filtering': n_CM_after_filtering,
-    'n_PCM_in_list_after_filtering': len(PCM_filtered),
-    
-    'n_CM_in_samps': len(CM_in_samps), 
-    'n_KCM_in_samps': len(KCM_in_samps), 
-    'n_PCM_in_samps': len(PCM_in_samps), 
-    
-    'n_KRM_in_samps': len(KRM_in_samps), 
-    'n_PRM_in_samps': len(PRM_filtered),
-    
-    "n_samps_PRM": len(samps_PRM), 
-    
-    "n_samps_CM": len(samps_CM), 
-    "n_samps_CM_and_KRM": len(samps_CM_and_KRM),
-    "n_samps_CM_and_no_KRM": len(samps_CM_and_no_KRM), 
-    "n_samps_CM_and_no_KRM_and_PRM": len(samps_CM_and_no_KRM_and_PRM), 
-    "n_samps_CM_and_no_KRM_and_no_PRM": len(samps_CM_and_no_KRM_and_no_PRM)}
-
-    print()
-    print("summary for", drug_of_interest)
-    for x in summary_dict:
-        print(x, summary_dict[x])
-    print()
-
     if len(samps_PRM) > 0:
         with open(PRM_samples_file, 'w') as f:
             for samp in samps_PRM:
@@ -477,31 +406,27 @@ def main(args):
             writer.writerow(binary_table[row])
     clean_file(binary_table_file)
 
-    with open(summary_file, 'w') as f:
-        writer = csv.DictWriter(f, fieldnames = summary_dict.keys())
-        writer.writeheader()
-        writer.writerow(summary_dict)
-    clean_file(summary_file)
-
 
 parser = argparse.ArgumentParser(description='get novel potential resistance mutations from compensatory mutations',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+# in
+parser.add_argument('--do-lineage', type = str, help = 'run script analysing lineage? yes = 1, 0 = no')
+parser.add_argument('--suffix', default = '.results.json', type = str, help = 'suffix of json files in tbprofiler_results_dir')
+parser.add_argument('--tbprofiler-results-dir', default = '', type = str, help = 'directory of tbprofiler results containing one json per sample')
 parser.add_argument('--drug-of-interest', default = '', type = str, help = 'drug of interest to mutations e.g. "isoniazid"')
-parser.add_argument('--PCM-file', default = '', type = str, help = 'csv; first column = list of mutations e.g. c.-51G>A')
+parser.add_argument('--CM-file', default = '', type = str, help = 'csv; first column = list of mutations e.g. c.-51G>A')
+parser.add_argument('--KCM-file', default = '', type = str, help = 'csv of all known compensatory mutations; https://github.com/GaryNapier/pipeline/blob/main/db/compensatory_mutations.csv')
 parser.add_argument('--metadata-file', default = '', type = str, help = 'csv of metadata')
 parser.add_argument('--tbdb-file', default = '', type = str, help = 'csv from https://github.com/jodyphelan/tbdb/blob/master/tbdb.csv')
 parser.add_argument('--CM-RM-assoc-file', default = '', type = str, help = 'csv associating CM genes to RM genes by drug, e.g. isoniazid, ahpC, katG')
 parser.add_argument('--drtypes-file', default = '', type = str, help = 'json converting old WHO drug resistance types to new ones; https://github.com/GaryNapier/pipeline/blob/main/db/dr_types.json')
-parser.add_argument('--KCM-file', default = '', type = str, help = 'csv of all known compensatory mutations; https://github.com/GaryNapier/pipeline/blob/main/db/compensatory_mutations.csv')
-parser.add_argument('--tbprofiler-results-dir', default = '', type = str, help = 'directory of tbprofiler results containing one json per sample')
 parser.add_argument('--vars-exclude-file', default = '', type = str, help = 'csv of gene,mutation to exclude. No header')
+# out
+parser.add_argument('--CM-stats-file', default = '', type = str, help = 'name of output file of CMs and their stats')
 parser.add_argument('--PRM-stats-file', default = '', type = str, help = 'name of output file of PRMs and their stats')
-parser.add_argument('--PCM-stats-file', default = '', type = str, help = 'name of output file of PCMs and their stats')
 parser.add_argument('--PRM-samples-file', default = '', type = str, help = 'name of output file of samples with PRMs')
 parser.add_argument('--binary-table-file', default = '', type = str, help = 'name of output file of samples and all their types of mutation - CM, KRM, PRM, other')
-parser.add_argument('--summary-file', default = '', type = str, help = 'name of output file of CM, KRM etc counts')
-parser.add_argument('--suffix', default = '.results.json', type = str, help = 'suffix of json files in tbprofiler_results_dir')
-parser.add_argument('--do-lineage', type = str, help = 'run script analysing lineage? yes = 1, 0 = no')
+# parser.add_argument('--summary-file', default = '', type = str, help = 'name of output file of CM, KRM etc counts')
 
 parser.set_defaults(func=main)
 
--- shell_scripts/comp_mut_master_script.sh
+++ shell_scripts/comp_mut_master_script.sh
@@ -50,30 +50,30 @@ vcf_dir=vcf/
 
 # Existing files
 main_tb_metadata_file=${main_metadata_dir}tb_data_18_02_2021.csv
-KCM_file=${database_dir}compensatory_mutations.csv 
+KCM_file=${database_dir}compensatory_mutations.csv
+PCM_file=${local_metadata_dir}found_PCM.csv
 tbdb_file=${tbdb_dir}tbdb.csv
 CM_RM_assoc_file=${local_metadata_dir}CM_RM_gene_assoc.csv
 dr_types_file=${database_dir}dr_types.json
 vars_exclude_file=${local_metadata_dir}var_exclude_comp_mut.csv
-tc_file=${local_metadata_dir}${drug_of_interest}_tc.txt
+# tc_file=${local_metadata_dir}${drug_of_interest}_tc.txt
 
 # Created files
 head_metadata_file=${main_metadata_dir}head_metadata.csv # testing
 sample_list=test_sample_list.txt
 temp_json_files_list=files.tmp
 # find_comp_mutations.py files
-PCM_data_file=${results_dir}${drug_of_interest}_PCM_data.txt
+# PCM_data_file=${results_dir}${drug_of_interest}_PCM_data.txt
 # filter_PCM.R files
 PCM_model_results_file=${results_dir}${drug_of_interest}_PCM_model_results.csv
 # clean_PCM files
-PCM_merged_file=${results_dir}${drug_of_interest}_PCM_merged.csv
+PCM_KCM_merged_file=${results_dir}${drug_of_interest}_PCM_KCM_merged.csv
 # comp_mut2res_mut.py files
 PRM_stats_file=${results_dir}${drug_of_interest}_PRM_stats.csv
 PCM_stats_file=${results_dir}${drug_of_interest}_PCM_stats.csv
 PRM_samples_file=${results_dir}${drug_of_interest}_PRM_samps.txt
-# samples_for_vcf_file=${results_dir}${drug_of_interest}_PRM_samps.txt
 binary_table_file=${results_dir}${drug_of_interest}_binary_table.csv
-summary_file=${results_dir}${drug_of_interest}_summary.csv
+# summary_file=${results_dir}${drug_of_interest}_summary.csv
 # tree_pipeline.sh files
 gvcf_file_suffix=.g.vcf.gz 
 multi_samp_vcf=${vcf_dir}${drug_of_interest}.val.gt.g.vcf.gz
@@ -88,65 +88,67 @@ tree_png=${results_dir}${drug_of_interest}_tree.png
 # Find all novel comp mutations for drug of interest
 # ---------------------------------------------------
 
+# echo ""
+# echo " --- PULLING COMPENSATORY MUTATIONS DATA FOR ${drug_of_interest} RUNNING python_scripts/find_PCM.py --- "
+# echo ""
+# python python_scripts/find_PCM.py \
+# --metadata-file ${main_tb_metadata_file} \
+# --KCM-file ${KCM_file} \
+# --drug-of-interest ${drug_of_interest} \
+# --id-key ${id_key} \
+# --tbp-results ${tbp_results_dir} \
+# --outfile ${PCM_data_file}
+
+
+# if [ ${lineage} == 1 ]; then 
+
+#     echo "LINEAGE = ${lineage}"
+
+#     # ------------------------------------------------------------------------------------------------------------------------------------
+#     # Run filter_PCM.R - run GLM models to see if DST is significantly predicted against presence of each mutation and lineage
+#     # ------------------------------------------------------------------------------------------------------------------------------------
+
+#     echo ""
+#     echo " --- FILTERING POTENTIAL NEW COMPENSATORY MUTATIONS - RUNNING r_scripts/filter_PCM.R --- "
+#     echo ""
+#     Rscript r_scripts/filter_PCM.R \
+#     --metadata_file ${main_tb_metadata_file} \
+#     --PCM_data_file ${PCM_data_file} \
+#     --drug_of_interest ${drug_of_interest} \
+#     --outfile ${PCM_model_results_file} 
+
+#     # --------------------------------------------------------------------------------------------------------
+#     # Concatenate output of filter_PCM.R with previous analyses of potential comp. mutations by TC
+#     # --------------------------------------------------------------------------------------------------------
+
+#     echo ""
+#     echo " --- CLEANING AND MERGING ${tc_file} AND ${PCM_model_results_file} - RUNNING clean_PCM.R"
+#     echo ""
+#     Rscript r_scripts/clean_PCM.R \
+#     --drug_of_interest ${drug_of_interest} \
+#     --tc_file ${tc_file} \
+#     --gn_results_file ${PCM_model_results_file} \
+#     --KCM_file ${KCM_file} \
+#     --outfile ${PCM_merged_file}
+
+# else
+
+    # echo "LINEAGE = ${lineage}"
+
 echo ""
-echo " --- PULLING COMPENSATORY MUTATIONS DATA FOR ${drug_of_interest} RUNNING python_scripts/find_PCM.py --- "
+# echo " --- MERGING ${tc_file} AND ${PCM_data_file} - RUNNING merge_PCM.R"
+echo " --- MERGING ${PCM_file} AND ${KCM_file} - RUNNING merge_PCM.R"
 echo ""
-python python_scripts/find_PCM.py \
---metadata-file ${main_tb_metadata_file} \
---KCM-file ${KCM_file} \
---drug-of-interest ${drug_of_interest} \
---id-key ${id_key} \
---tbp-results ${tbp_results_dir} \
---outfile ${PCM_data_file}
 
+Rscript r_scripts/merge_PCM.R \
+--drug_of_interest ${drug_of_interest} \
+# --tc_file ${tc_file} \
+# --PCM_data_file ${PCM_data_file} \
+--KCM_file ${KCM_file} \
+--PCM_file ${PCM_file}
+--outfile ${PCM_KCM_merged_file}
 
-if [ ${lineage} == 1 ]; then 
-
-    echo "LINEAGE = ${lineage}"
-
-    # ------------------------------------------------------------------------------------------------------------------------------------
-    # Run filter_PCM.R - run GLM models to see if DST is significantly predicted against presence of each mutation and lineage
-    # ------------------------------------------------------------------------------------------------------------------------------------
-
-    echo ""
-    echo " --- FILTERING POTENTIAL NEW COMPENSATORY MUTATIONS - RUNNING r_scripts/filter_PCM.R --- "
-    echo ""
-    Rscript r_scripts/filter_PCM.R \
-    --metadata_file ${main_tb_metadata_file} \
-    --PCM_data_file ${PCM_data_file} \
-    --drug_of_interest ${drug_of_interest} \
-    --outfile ${PCM_model_results_file} 
-
-    # --------------------------------------------------------------------------------------------------------
-    # Concatenate output of filter_PCM.R with previous analyses of potential comp. mutations by TC
-    # --------------------------------------------------------------------------------------------------------
-
-    echo ""
-    echo " --- CLEANING AND MERGING ${tc_file} AND ${PCM_model_results_file} - RUNNING clean_PCM.R"
-    echo ""
-    Rscript r_scripts/clean_PCM.R \
-    --drug_of_interest ${drug_of_interest} \
-    --tc_file ${tc_file} \
-    --gn_results_file ${PCM_model_results_file} \
-    --KCM_file ${KCM_file} \
-    --outfile ${PCM_merged_file}
-
-else
-
-    echo "LINEAGE = ${lineage}"
-
-    echo ""
-    echo " --- MERGING ${tc_file} AND ${PCM_data_file} - RUNNING merge_PCM.R"
-    echo ""
-
-    Rscript r_scripts/merge_PCM.R \
-    --drug_of_interest ${drug_of_interest} \
-    --tc_file ${tc_file} \
-    --PCM_data_file ${PCM_data_file} \
-    --KCM_file ${KCM_file} \
-    --outfile ${PCM_merged_file}
-
-fi
+# fi
 
 # ----------------------------------------------------------------------------------------
 # Filter potential novel compensatory mutations with tbprofiler critera 
@@ -157,22 +159,23 @@ echo ""
 echo " --- GETTING POTENTIAL NEW RESISTANCE MUTATIONS FOR ${drug_of_interest}; RUNNING python_scripts/comp_mut2res_mut.py --- "
 echo ""
 python python_scripts/comp_mut2res_mut.py \
+# in
+--do-lineage ${lineage}
 --drug-of-interest ${drug_of_interest} \
---PCM-file ${PCM_merged_file} \
+--KCM-file ${KCM_file} \
+--CM-file ${PCM_KCM_merged_file} \
+--CM-RM-assoc-file ${CM_RM_assoc_file} \
 --metadata-file ${main_tb_metadata_file} \
 --tbdb-file ${tbdb_file} \
---CM-RM-assoc-file ${CM_RM_assoc_file} \
 --drtypes-file ${dr_types_file} \
---KCM-file ${KCM_file} \
 --tbprofiler-results-dir ${tbp_results_dir} \
 --vars-exclude-file ${vars_exclude_file} \
+# out
 --PRM-stats-file ${PRM_stats_file} \
 --PCM-stats-file ${PCM_stats_file} \
 --PRM-samples-file ${PRM_samples_file} \
 --binary-table-file ${binary_table_file} \
---summary-file ${summary_file} \
---do-lineage ${lineage}
-
+# --summary-file ${summary_file} \
 
 if [ ! -f ${PRM_samples_file} ]; then 
 
